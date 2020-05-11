ac6411e257393a4c52b9cb75c62d1884d9883012
Stéphane B <stillfury@hotmail.com>
2020-05-10 21:39:42 -0400

    add test for post endpoint

---
+++ src/routes.py
@@ -0,0 +1,41 @@
+from flask import request, flash
+import os
+from werkzeug.utils import secure_filename
+import base64
+import cv2
+
+
+def configure_routes(app):
+
+    @app.route('/', methods=['POST'])
+    def uploadCurl():
+
+        if 'image' not in request.files:
+            flash('No file part')
+            return 'Please include an image.'
+
+        file = request.files['image']
+        filename = secure_filename(file.filename)  # save file
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        image = cv2.imread(filepath)
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Write grayscale image to /tmp
+        cv2.imwrite(os.path.join(
+            app.config['UPLOAD_FOLDER'], 'gray.jpg'), gray)
+
+        with open('/tmp/gray.jpg', 'rb') as imageFile:
+            str = base64.b64encode(imageFile.read())
+            encoded_img = str.decode('utf-8')
+
+        return {
+            'body': encoded_img
+        }
+
+
+# f = request.files['the_file']
+# content = f.stream.read()
+# if isinstance(content, str): print('content is str')
+# content = content.decode('utf-8')
+# if isinstance(content, unicode): print('content is unicode')
