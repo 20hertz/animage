2f8ad45c398908e9ede5283c450c7de45e3e01d4
Stéphane B <stillfury@hotmail.com>
2020-06-29 21:49:02 -0400

    try new approach to enable cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 import flask_cors
-import os
 from .config import CONFIGS
-
-CONFIG_NAME = os.getenv("FLASK_ENV")
 
 
 def create_app(config_name):
     app = Flask(__name__)
     flask_cors.CORS(app)
-    app.config.from_object(CONFIGS[config_name])
+    app.config.from_object(CONFIGS["development"])
 
     register_blueprint(app)
 
@@ -20,6 +17,3 @@ def register_blueprint(app):
     from app.service.transform import transform_blueprint
 
     app.register_blueprint(transform_blueprint)
-
-
-app = create_app(CONFIG_NAME)
