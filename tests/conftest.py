62032a33be27e79048314e1aac120aacc684d530
Stéphane B <stillfury@hotmail.com>
2020-07-23 21:46:25 -0400

    create a decorator to reuse request validation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,36 @@
+# import pytest
+# from flask import Flask
+
+
+# @pytest.fixture()
+# def app() -> Flask:
+#     from app import create_app
+
+#     return create_app()
+
+
+# import pytest
+
+# from app import create_app
+
+
+# @pytest.fixture
+# def app():
+#     app = create_app()
+#     return app.app
+
+
 import pytest
-from flask import Flask
+from app import create_app
 
 
-@pytest.fixture()
-def app() -> Flask:
-    from app import create_app
+@pytest.fixture
+def app():
+    app = create_app()
+    yield app
 
-    return create_app()
+
+@pytest.fixture
+def client(app):
+    client = app.test_client()
+    yield client
